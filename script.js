dbc154391abd1c10767cdb06389e10b1f9ce1a7b
Matthew Lee <matthewlee626@gmail.com>
2020-04-05 08:48:11 -0700

    changed data

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    getNews();
+};
+
+function getNews(){
+    var url = 'http://newsapi.org/v2/top-headlines?' +
+            'country=us&' +
+            'apiKey=948434005607451495f706ca0792f4b9';
+    var req = new Request(url);
+    fetch(req)
+        .then(function(response) {
+            console.log(response.json());
+        })
+        
+}
